4dec44d6f3ad04d78f8ab4e1e31b654f200d866f
Swarnim Shamgaonkar <103427124+Swarnim31@users.noreply.github.com>
2025-07-18 15:11:11 +0530

    Update data.ts

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -98,7 +98,7 @@ export const mockTeam = [
     role: "Web Development Co-Lead",
     imageUrl: "https://res.cloudinary.com/startup-grind/image/upload/c_fill,w_250,h_250,g_center/c_fill,dpr_2.0,f_auto,g_center,q_auto:good/v1/gcs/platform-data-goog/avatars/swarnim_shamgaonkar_MunLauF.jpg",
     imageHint: "person portrait",
-    linkedinUrl: "#",
+    linkedinUrl: "https://www.linkedin.com/in/swarnim-shamgaonkar-b725472b1/",
   },
   {
     id: "4",
